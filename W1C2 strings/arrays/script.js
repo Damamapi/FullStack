f39c3be4902124feb50f09aeefe361b7b718e949
Damamapi <ddamamapi@gmail.com>
2020-01-21 11:50:39 -0500

    W1C2

--- W1C2 strings/arrays/script.js
+++ W1C2 strings/arrays/script.js
@@ -1,5 +1,32 @@
-function capital(s) {
-    let first = s.charAt(0).toUpperCase();
-    let rest = s.substr(1, s.length).toLowerCase();
-    return first + rest;
+function ex(s) {
+    let arr = s.split(" ");
+    console.log(arr.length);
+    let r = "";
+    for(let i = 0; i < arr.length; i++) {
+        console.log("r, capitalL(arr[i])");
+        r += (reverseString(capitalL(arr[i])) + " ");
+    }
+    console.log("after");
+    return r;
+}
+
+function reverseString(s) {
+    //str to arr - split / arr to str - join
+
+    // let arr = s.split('');
+    // let r = "";
+    // for(let i = arr.length - 1; i >= 0; i--) {
+    //     r += arr[i];
+    // }
+    // return r;
+
+    return s.split('').reverse().join('');
+}
+
+function capitalF(s) {
+    return s.charAt(0).toUpperCase() + s.substr(1).toLowerCase();
+}
+
+function capitalL(s) {
+    return s.substr(0, s.length - 1).toLowerCase() + s.charAt(s.length - 1).toUpperCase();
 }
